c6a8266c312bf26e5e1c6c45999d3060724f49ab
qenvio <enrique.vidal@crg.eu>
2017-02-06 12:09:12 +0100

    initial commit

---
+++ R/data.R
@@ -0,0 +1,80 @@
+#' Genomic biases for hg38
+#'
+#' A \code{data.frame} containing the mappability and CG proportion
+#' of the human genome (hg38) in 1 Kbp bins
+#'
+#' @format A data frame with 3031043 rows and 4 variables:
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{position}{start postion of the genomic bin}
+#'   \item{map}{mappability as computed by gem}
+#'   \item{cg}{cg content as computed by bedtools}
+#' }
+"bias_hg38"
+
+#' Genomic biases for mm10
+#'
+#' A \code{data.frame} containing the mappability and CG
+#' proportion of the mouse genome (mm10) in 1 Kbp bins
+#'
+#' @format A data frame with 2617314 rows and 4 variables:
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{position}{start postion of the genomic bin}
+#'   \item{map}{mappability as computed by gem}
+#'   \item{cg}{cg content as computed by bedtools}
+#' }
+"bias_mm10"
+
+#' Number of restriction enzyme sites for hg38
+#'
+#' A \code{data.frame} containing the number of
+#' restriction enzyme sites (RES) of the human genome
+#' (hg38) in 1 Kbp bins. There is one column per
+#' restriction enzyme
+#'
+#' @format A data frame with 3031043 rows and 7 variables:
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{position}{start postion of the genomic bin}
+#'   \item{BspHI}{number of RES for BspHI}
+#'   \item{DpnII}{number of RES for DpnII}
+#'   \item{HindIII}{number of RES for HindIII}
+#'   \item{MspI}{number of RES for MspI}
+#'   \item{NcoI}{number of RES for NcoI}
+#' }
+#' @details Please note that MboI presents the same motif
+#' as DpnII, so use that column corerspondingly
+"enzymes_hg38"
+
+#' Number of restriction enzyme sites for mm10
+#'
+#' A \code{data.frame} containing the number of
+#' restriction enzyme sites (RES) of the human genome
+#' (mm10) in 1 Kbp bins. There is one column per
+#' restriction enzyme
+#'
+#' @format A data frame with 2617314 rows and 7 variables:
+#' \describe{
+#'   \item{chr}{chromosome}
+#'   \item{position}{start postion of the genomic bin}
+#'   \item{BspHI}{number of RES for BspHI}
+#'   \item{DpnII}{number of RES for DpnII}
+#'   \item{HindIII}{number of RES for HindIII}
+#'   \item{MspI}{number of RES for MspI}
+#'   \item{NcoI}{number of RES for NcoI}
+#' }
+#' @details Please note that MboI presents the same motif
+#' as DpnII, so use that column corerspondingly
+"enzymes_mm10"
+
+#' HiC contact matrix for Rao's HIC077 sample at 1 Mbp
+#' resolution
+#'
+#' A sparse \code{dgCMatrix} containing the HiC contacts
+#' for Rao's sample HIC077 (SRR1658706) at 1 Mbp resolution
+#'
+#' @format A 3044 x 3044 sparse \code{dgCMatrix} with the
+#' bin ids as dimnames
+#' @source \url{http://dx.doi.org/10.1016/j.cell.2014.11.021}
+"mat_kbm7"
